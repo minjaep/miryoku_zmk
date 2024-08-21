f0e8d251896f966333f73a0665a30e2d0a8ec8ad
Minjae <mr.minjae.park@gmail.com>
2024-08-21 14:12:42 -0500

    add: soft off on left

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -74,7 +74,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC       K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &soft_off \
+U_MT(&soft_off, &kp ESC)       K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &soft_off \
 &u_caps_word  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &tog U_FUN \
 &tog U_EXTRA  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &tog U_BUTTON \
                K32  K33  K34       K35  K36  K37
