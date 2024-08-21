6c846225067baf447f2cf6779698e40d11fe413e
Minjae <mr.minjae.park@gmail.com>
2024-08-21 13:56:35 -0500

    fix: typo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,6 +5,34 @@
 #define U_LANG &u_macro_lang
 #define U_A_LANG &u_macro_and_lang
 
+#if !defined (MIRYOKU_LAYER_LIST)
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "ENG") \
+MIRYOKU_X(EXTRA,  "KOR") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "AND") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun")
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+
+#endif
+
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
@@ -33,7 +61,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            \
 &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            \
 &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            \
-&trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            U_A_LANG,          &trans,            &trans,            \
+&trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            U_A_LANG,          &trans,            &trans
 
 
 #if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
